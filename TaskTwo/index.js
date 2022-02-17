0b6fa870a58311273a55de89c03ecbf0f884ba15
harish anchuri <harish.anchuri@gmail.com>
2022-02-17 09:32:00 +0530

    task two

---
+++ TaskTwo/index.js
@@ -0,0 +1,12 @@
+function function1(){
+    document.getElementById('demo').style.color="#ff0000";
+}
+function function2(){
+    document.getElementById('demo').style.color="#000000";
+}
+function func1(){
+    document.getElementById('demo2').style.color="#ff0000";
+}
+function func2(){
+    document.getElementById('demo2').style.color="#000000";
+}
